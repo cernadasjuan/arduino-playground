4e588d0799dd4eb1cb96ea094aa632b84319af74
Juan Cernadas <juan@onready.com.ar>
2021-05-04 23:23:01 -0300

    format code

--- push-button/push-button.ino
+++ push-button/push-button.ino
@@ -1,15 +1,15 @@
-/**
+/*
  * This code turns on/off a led by pressing a button
- **/
+ */
 
 #include <Arduino.h>
 
 const uint8_t BUTTON_PIN = 2;
 const uint8_t LED_PIN = 12;
 
-/**
+/*
  * This code runs once
- **/
+ */
 void setup() {
   // Init button
   pinMode(BUTTON_PIN, INPUT);
@@ -21,9 +21,9 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-/**
+/*
  * This code runs forever after setup
- **/
+ */
 void loop() {
   int buttonState = digitalRead(BUTTON_PIN);
   if (buttonState == HIGH) {
